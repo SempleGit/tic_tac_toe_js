71de56c213a8719c2ecbf31784b12ca0df55d2a1
Daniel Semple <dsemple.isu@gmail.com>
2020-09-08 11:31:30 -0500

    updated styling

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
 //module to create the gameBoard and handle plays.
 const gameBoard = (() => {
-    let board = [];
-    for (let i = 0; i < 9; i++) {
-        board.push(i);
+    let board = [[0, 1, 2], [0, 1, 2], [0, 1, 2]];
+    // for (let i = 0; i < 3; i++) {
+    //     board.push(i);
+    // }
+    const play = (r, col, marker) => {
+        console.log(board[r][col])
+        board[r][col] = marker;
     }
-    const play = (space, marker) => board[space] = marker;
     const getBoard = () => board;
     return { play, getBoard } ;
 })();
@@ -16,10 +19,21 @@ const player = (name, marker) => {
     return { getMarker, getName };
 };
 
+let test; 
+
 renderBoard = () => {
     const gameSquare = document.querySelectorAll(".gameSquare");
-    for (let i = 0; i < gameBoard.getBoard().length; i++) {
-        gameSquare[i].textContent = gameBoard.getBoard()[i];
-        gameSquare[i].setAttribute(["data-playReference"], i);
+    // for (let i = 0; i < gameBoard.getBoard().length; i++) {
+    //     gameSquare[i].textContent = gameBoard.getBoard()[i];
+    //     gameSquare[i].setAttribute(["data-playReference"], i);
+    // }
+    let i = 0;
+    gameBoard.getBoard().forEach(square => test = square);
+    for (let row = 0; row < gameBoard.getBoard().length; row++) {
+        for (let col = 0; col < gameBoard.getBoard()[row].length; col++) {
+            gameSquare[i].textContent = gameBoard.getBoard()[row][col];
+            gameSquare[i].setAttribute(["data-playReference"], `${row},${col}`);
+            i++;
+        }
     }
 }
