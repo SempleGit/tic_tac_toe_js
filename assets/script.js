f26d8d5ef6960cd3e034fd60997b3bd4df8af5d2
Daniel Semple <dsemple.isu@gmail.com>
2020-09-08 21:32:50 -0500

    updating js

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,48 @@
 //module to create the gameBoard and handle plays.
 const gameBoard = (() => {
-    let board = [[0, 1, 2], [0, 1, 2], [0, 1, 2]];
-    // for (let i = 0; i < 3; i++) {
-    //     board.push(i);
-    // }
+    const row = [0, 1, 2];
+    const board = [row.slice(), row.slice(), row.slice()];
+   
+    const initializeBoard =(board => {
+        const gameSquare = document.querySelectorAll(".gameSquare");
+        for (let row = 0; row < board.length; row++) {
+            for (let col = 0; col < board[row].length; col++) {
+                const i = row * board.length + col;
+                gameSquare[i].setAttribute(["data-playReference"], `${row},${col}`);
+            }
+        }
+    })(board);
+
     const play = (r, col, marker) => {
-        console.log(board[r][col])
         board[r][col] = marker;
+        renderBoard();
     }
+
     const getBoard = () => board;
     return { play, getBoard } ;
 })();
 
+
+
 //player factory function
 const player = (name, marker) => {
     const getMarker = () => marker;
-    const getName = () => getName;
+    const getName = () => name;
     return { getMarker, getName };
 };
 
-let test; 
-
+//displays the board after every play
 renderBoard = () => {
     const gameSquare = document.querySelectorAll(".gameSquare");
-    // for (let i = 0; i < gameBoard.getBoard().length; i++) {
-    //     gameSquare[i].textContent = gameBoard.getBoard()[i];
-    //     gameSquare[i].setAttribute(["data-playReference"], i);
-    // }
-    let i = 0;
-    gameBoard.getBoard().forEach(square => test = square);
     for (let row = 0; row < gameBoard.getBoard().length; row++) {
         for (let col = 0; col < gameBoard.getBoard()[row].length; col++) {
-            gameSquare[i].textContent = gameBoard.getBoard()[row][col];
-            gameSquare[i].setAttribute(["data-playReference"], `${row},${col}`);
-            i++;
+            const i = row * gameBoard.getBoard().length + col;
+            gameSquare[i].textContent = gameBoard.getBoard()[row][col] == "X" || gameBoard.getBoard()[row][col] == "O" ? gameBoard.getBoard()[row][col] : "";
         }
     }
 }
+
+
+
+const player1 = player("player1", "X");
+const player2 = player("player2", "O");
